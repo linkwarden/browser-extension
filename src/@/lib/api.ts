7283a157b0bb4a9d4db67a2bdf6a546775c0a171
Jordan Higuera Higuera <jordandark123@gmail.com>
2023-08-09 20:26:46 -0700

    add todos

--- src/@/lib/api.ts
+++ src/@/lib/api.ts
@@ -1,6 +1,7 @@
 import { DefaultConfig } from './config.ts';
 
 // Just a placeholder for now, until apis routes and token auth are implemented
+// TODO: Implement apis routes and token auth, if not possible on just queries for react query, I don't really want to manage it on a separate class
 export class LinkwardenApi {
   configuration: DefaultConfig;
 
