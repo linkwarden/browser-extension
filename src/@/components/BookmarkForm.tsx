de87e1ad3c2d2d35b37cd90dc1c453f535095c61
Jordan Higuera Higuera <jordandark123@gmail.com>
2023-08-13 09:52:26 -0700

    handle errors if the site isn't found

--- src/@/components/BookmarkForm.tsx
+++ src/@/components/BookmarkForm.tsx
@@ -111,7 +111,7 @@ const BookmarkForm = () => {
   }, [form]);
 
 
-  const { isLoading: loadingCollections, data: collections } = useQuery({
+  const { isLoading: loadingCollections, data: collections, error } = useQuery({
     queryKey: ['collections'],
     queryFn: async () => {
       const config = await getConfig();
@@ -155,6 +155,7 @@ const BookmarkForm = () => {
     <div>
       <Form {...form}>
         <form onSubmit={handleSubmit(e => onSubmit(e))} className='space-y-3 py-1'>
+          {error ? <p className='text-red-600'>There was an error make sure the site is available!.</p> : null}
           <FormField control={control} name='url' render={({ field }) => (
             <FormItem>
               <FormLabel>URL</FormLabel>
@@ -182,7 +183,7 @@ const BookmarkForm = () => {
                     <SelectItem value='Unnamed Collection'>Loading collections...</SelectItem>
                   ) : (
                     <>
-                      {collections.response.map((collection: {
+                      {collections?.response?.map((collection: {
                         id: number;
                         name: string
                       }) => (
