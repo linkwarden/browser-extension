5a4ff2ff5448d2c39d7d47341df86caca359bdbf
Daniel <daniel31x13@gmail.com>
2023-08-22 01:38:42 -0400

    prettier formatting + updated the api routes + some minor changes

--- src/@/lib/actions/tags.ts
+++ src/@/lib/actions/tags.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export async function getTags(baseUrl: string) {
-  const url = `${baseUrl}/api/routes/tags`;
+  const url = `${baseUrl}/api/tags`;
   return await axios.get(url);
 }
