3b0f5eb44582001816f2ecb831328bd1a654d09c
Daniel <daniel31x13@gmail.com>
2023-08-22 01:38:42 -0400

    prettier formatting + updated the api routes + some minor changes

--- src/@/lib/actions/collections.ts
+++ src/@/lib/actions/collections.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getCollections(baseUrl: string) {
-  const url = `${baseUrl}/api/routes/collections`;
+  const url = `${baseUrl}/api/collections`;
 
   return await axios.get(url);
 }
