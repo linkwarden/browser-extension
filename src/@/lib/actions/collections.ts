3e57ea26f04f5d96699f55b44b0238e531439fa1
Jordan Higuera Higuera <jordandark123@gmail.com>
2024-04-21 11:49:30 -0700

    feat: sorted collections and tags

--- src/@/lib/actions/collections.ts
+++ src/@/lib/actions/collections.ts
@@ -1,9 +1,23 @@
 import axios from 'axios';
 
+interface ResponseCollections {
+  color: string;
+  createdAt: string;
+  description: string;
+  id: number;
+  isPublic: boolean;
+  members: never[]; // Assuming members can be of any type, adjust as necessary
+  name: string;
+  ownerId: number;
+  parent: null | never; // Assuming parent can be of any type or null, adjust as necessary
+  parentId: null | number; // Assuming parentId can be null or a number
+  updatedAt: string;
+}
+
 export async function getCollections(baseUrl: string, apiKey: string) {
   const url = `${baseUrl}/api/v1/collections`;
 
-  return await axios.get(url, {
+  return await axios.get<{ response: ResponseCollections[] }>(url, {
     headers: {
       Authorization: `Bearer ${apiKey}`,
     },
