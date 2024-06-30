d3d7d31c350de7681b76205e7330d1132e732db5
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-06-30 19:09:16 -0400

    Added support for showing a different icon when the user navigates to a link that is already saved

--- src/@/lib/actions/links.ts
+++ src/@/lib/actions/links.ts
@@ -14,19 +14,23 @@ export async function postLinkFetch(baseUrl: string, data: bookmarkFormValues) {
     method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
   });
 }
 
-export async function updateLinkFetch(baseUrl: string, id: number, data: bookmarkFormValues) {
+export async function updateLinkFetch(
+  baseUrl: string,
+  id: number,
+  data: bookmarkFormValues
+) {
   const url = `${baseUrl}/api/v1/links/${id}`;
 
   return await fetch(url, {
     method: 'PUT',
     body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
   });
 }
@@ -41,6 +45,14 @@ export async function deleteLinkFetch(baseUrl: string, id: number) {
 
 export async function getLinksFetch(baseUrl: string) {
   const url = `${baseUrl}/api/v1/links`;
+  const response = await fetch(url);
+  return await response.json();
+}
+
+export async function getLinkFetch(baseUrl: string, linkUrl: string) {
+  const url =
+    `${baseUrl}/api/v1/links` +
+    `?searchByUrl=true&searchQueryString=${linkUrl}`;
   const response = await fetch(url);
   return await response.json();
 }
--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,15 +1,27 @@
 import { getBrowser, getCurrentTabInfo } from '../../@/lib/utils.ts';
 import BookmarkTreeNode = chrome.bookmarks.BookmarkTreeNode;
 import { getConfig, isConfigured } from '../../@/lib/config.ts';
-import { deleteLinkFetch, postLinkFetch, updateLinkFetch } from '../../@/lib/actions/links.ts';
+import {
+  deleteLinkFetch,
+  postLinkFetch,
+  updateLinkFetch,
+  getLinkFetch,
+} from '../../@/lib/actions/links.ts';
 import {
   bookmarkMetadata,
-  deleteBookmarkMetadata, getBookmarkMetadataByBookmarkId, getBookmarkMetadataByUrl, getBookmarksMetadata,
+  deleteBookmarkMetadata,
+  getBookmarkMetadataByBookmarkId,
+  getBookmarkMetadataByUrl,
+  getBookmarksMetadata,
   saveBookmarkMetadata,
 } from '../../@/lib/cache.ts';
 import ContextType = chrome.contextMenus.ContextType;
 import OnClickData = chrome.contextMenus.OnClickData;
-import { getCsrfTokenFetch, getSessionFetch, performLoginOrLogoutFetch } from '../../@/lib/auth/auth.ts';
+import {
+  getCsrfTokenFetch,
+  getSessionFetch,
+  performLoginOrLogoutFetch,
+} from '../../@/lib/auth/auth.ts';
 import OnInputEnteredDisposition = chrome.omnibox.OnInputEnteredDisposition;
 
 const browser = getBrowser();
@@ -17,148 +29,163 @@ const browser = getBrowser();
 // This is the main functions that will be called when a bookmark is created, update or deleted
 // Won't work with axios xhr or something not supported by the browser
 
-browser.bookmarks.onCreated.addListener(async (_id: string, bookmark: BookmarkTreeNode) => {
-  try {
-    const { syncBookmarks, baseUrl, username, password, usingSSO } = await getConfig();
-    if (!syncBookmarks || !bookmark.url) {
-      return;
+browser.bookmarks.onCreated.addListener(
+  async (_id: string, bookmark: BookmarkTreeNode) => {
+    try {
+      const { syncBookmarks, baseUrl, username, password, usingSSO } =
+        await getConfig();
+      if (!syncBookmarks || !bookmark.url) {
+        return;
+      }
+      const session = await getSessionFetch(baseUrl);
+
+      // Check if the bookmark already exists in the server by checking the url so, it doesn't create duplicates
+      // I know, could use the method search from the api, but I want to avoid as much api specific calls as possible
+      // in case isn't supported, so I prefer to do it this way, if performance is an issue I will think of change to that.
+
+      const existingLink = await getBookmarkMetadataByUrl(bookmark.url);
+      if (existingLink) {
+        return;
+      }
+
+      if (!session && !usingSSO) {
+        const csrfToken = await getCsrfTokenFetch(baseUrl);
+
+        await performLoginOrLogoutFetch(
+          `${baseUrl}/api/v1/auth/callback/credentials`,
+          {
+            csrfToken: csrfToken,
+            callbackUrl: `${baseUrl}/api/v1/auth/callback`,
+            json: true,
+            redirect: false,
+            username: username,
+            password: password,
+          }
+        );
+      } else if (!session && usingSSO) {
+        return;
+      }
+
+      const newLink = await postLinkFetch(baseUrl, {
+        url: bookmark.url,
+        collection: {
+          name: 'Unorganized',
+        },
+        tags: [],
+        name: bookmark.title,
+        description: bookmark.title,
+      });
+
+      const newLinkJson = await newLink.json();
+      const newLinkUrl: bookmarkMetadata = newLinkJson.response;
+      newLinkUrl.bookmarkId = bookmark.id;
+
+      await saveBookmarkMetadata(newLinkUrl);
+    } catch (error) {
+      console.error(error);
     }
-    const session = await getSessionFetch(baseUrl);
-
-    // Check if the bookmark already exists in the server by checking the url so, it doesn't create duplicates
-    // I know, could use the method search from the api, but I want to avoid as much api specific calls as possible
-    // in case isn't supported, so I prefer to do it this way, if performance is an issue I will think of change to that.
-
-    const existingLink = await getBookmarkMetadataByUrl(bookmark.url);
-    if (existingLink) {
-      return;
-    }
-
-    if (!session && !usingSSO) {
-      const csrfToken = await getCsrfTokenFetch(baseUrl);
-
-      await performLoginOrLogoutFetch(`${baseUrl}/api/v1/auth/callback/credentials`, {
-        csrfToken: csrfToken,
-        callbackUrl: `${baseUrl}/api/v1/auth/callback`,
-        json: true,
-        redirect: false,
-        username: username,
-        password: password,
-      })
-    } else if (!session && usingSSO) {
-      return;
-    }
-
-    const newLink = await postLinkFetch(baseUrl, {
-      url: bookmark.url,
-      collection: {
-        name: "Unorganized",
-      },
-      tags: [],
-      name: bookmark.title,
-      description: bookmark.title,
-    });
-
-    const newLinkJson = await newLink.json()
-    const newLinkUrl: bookmarkMetadata = newLinkJson.response;
-    newLinkUrl.bookmarkId = bookmark.id;
-
-    await saveBookmarkMetadata(newLinkUrl)
-
-  } catch (error) {
-    console.error(error);
   }
-});
+);
 
-browser.bookmarks.onChanged.addListener(async (id: string, changeInfo: chrome.bookmarks.BookmarkChangeInfo) => {
-  try {
-    const { syncBookmarks, baseUrl, username, password, usingSSO } = await getConfig();
-    if (!syncBookmarks || !changeInfo.url) {
-      return;
+browser.bookmarks.onChanged.addListener(
+  async (id: string, changeInfo: chrome.bookmarks.BookmarkChangeInfo) => {
+    try {
+      const { syncBookmarks, baseUrl, username, password, usingSSO } =
+        await getConfig();
+      if (!syncBookmarks || !changeInfo.url) {
+        return;
+      }
+
+      const link = await getBookmarkMetadataByBookmarkId(id);
+
+      if (!link) {
+        return;
+      }
+
+      const session = await getSessionFetch(baseUrl);
+
+      if (!session && !usingSSO) {
+        const csrfToken = await getCsrfTokenFetch(baseUrl);
+
+        await performLoginOrLogoutFetch(
+          `${baseUrl}/api/v1/auth/callback/credentials`,
+          {
+            csrfToken: csrfToken,
+            callbackUrl: `${baseUrl}/api/v1/auth/callback`,
+            json: true,
+            redirect: false,
+            username: username,
+            password: password,
+          }
+        );
+      } else if (!session && usingSSO) {
+        return;
+      }
+
+      const updatedLink = await updateLinkFetch(baseUrl, link.id, {
+        url: changeInfo.url,
+        collection: {
+          name: 'Unorganized',
+        },
+        tags: [],
+        name: changeInfo.title,
+        description: changeInfo.title,
+      });
+
+      const updatedLinkJson = await updatedLink.json();
+      const newLinkUrl: bookmarkMetadata = updatedLinkJson.response;
+      newLinkUrl.bookmarkId = id;
+
+      await saveBookmarkMetadata(newLinkUrl);
+    } catch (error) {
+      console.error(error);
     }
-
-    const link = await getBookmarkMetadataByBookmarkId(id);
-
-    if (!link) {
-      return;
-    }
-
-    const session = await getSessionFetch(baseUrl);
-
-    if (!session && !usingSSO) {
-      const csrfToken = await getCsrfTokenFetch(baseUrl);
-
-      await performLoginOrLogoutFetch(`${baseUrl}/api/v1/auth/callback/credentials`, {
-        csrfToken: csrfToken,
-        callbackUrl: `${baseUrl}/api/v1/auth/callback`,
-        json: true,
-        redirect: false,
-        username: username,
-        password: password,
-      })
-    } else if (!session && usingSSO) {
-      return;
-    }
-
-    const updatedLink = await updateLinkFetch(baseUrl, link.id, {
-      url: changeInfo.url,
-      collection: {
-        name: "Unorganized",
-      },
-      tags: [],
-      name: changeInfo.title,
-      description: changeInfo.title,
-    });
-
-    const updatedLinkJson = await updatedLink.json()
-    const newLinkUrl: bookmarkMetadata = updatedLinkJson.response;
-    newLinkUrl.bookmarkId = id;
-
-    await saveBookmarkMetadata(newLinkUrl)
-
-  } catch (error) {
-    console.error(error);
   }
-});
+);
 
-browser.bookmarks.onRemoved.addListener(async (id: string, removeInfo: chrome.bookmarks.BookmarkRemoveInfo) => {
-  try {
-    const { syncBookmarks, baseUrl, username, password, usingSSO } = await getConfig();
-    if (!syncBookmarks || !removeInfo.node.url) {
-      return;
+browser.bookmarks.onRemoved.addListener(
+  async (id: string, removeInfo: chrome.bookmarks.BookmarkRemoveInfo) => {
+    try {
+      const { syncBookmarks, baseUrl, username, password, usingSSO } =
+        await getConfig();
+      if (!syncBookmarks || !removeInfo.node.url) {
+        return;
+      }
+      const link = await getBookmarkMetadataByBookmarkId(id);
+
+      if (!link) {
+        return;
+      }
+
+      const session = await getSessionFetch(baseUrl);
+
+      if (!session && !usingSSO) {
+        const csrfToken = await getCsrfTokenFetch(baseUrl);
+
+        await performLoginOrLogoutFetch(
+          `${baseUrl}/api/v1/auth/callback/credentials`,
+          {
+            csrfToken: csrfToken,
+            callbackUrl: `${baseUrl}/api/v1/auth/callback`,
+            json: true,
+            redirect: false,
+            username: username,
+            password: password,
+          }
+        );
+      } else if (!session && usingSSO) {
+        return;
+      }
+
+      await Promise.all([
+        deleteBookmarkMetadata(link.bookmarkId),
+        deleteLinkFetch(baseUrl, link.id),
+      ]);
+    } catch (error) {
+      console.error(error);
     }
-    const link = await getBookmarkMetadataByBookmarkId(id);
-
-    if (!link) {
-      return;
-    }
-
-    const session = await getSessionFetch(baseUrl);
-
-    if (!session && !usingSSO) {
-      const csrfToken = await getCsrfTokenFetch(baseUrl);
-
-      await performLoginOrLogoutFetch(`${baseUrl}/api/v1/auth/callback/credentials`, {
-        csrfToken: csrfToken,
-        callbackUrl: `${baseUrl}/api/v1/auth/callback`,
-        json: true,
-        redirect: false,
-        username: username,
-        password: password,
-      })
-    } else if (!session && usingSSO) {
-      return;
-    }
-
-    await Promise.all([
-      deleteBookmarkMetadata(link.bookmarkId),
-      deleteLinkFetch(baseUrl, link.id)
-    ])
-
-  } catch (error) {
-    console.error(error);
   }
-});
+);
 
 // This is for the context menus!
 // Example taken from: https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/api-samples/contextMenus/basic/sample.js
@@ -170,8 +197,12 @@ browser.contextMenus.onClicked.addListener(async (info, tab) => {
 });
 
 // A generic onclick callback function.
-async function genericOnClick(info: OnClickData, tab: chrome.tabs.Tab | undefined) {
-  const { syncBookmarks, baseUrl, username, password, usingSSO } = await getConfig();
+async function genericOnClick(
+  info: OnClickData,
+  tab: chrome.tabs.Tab | undefined
+) {
+  const { syncBookmarks, baseUrl, username, password, usingSSO } =
+    await getConfig();
   const configured = await isConfigured();
   if (!tab?.url || !tab?.title || !configured) {
     return;
@@ -200,14 +231,17 @@ async function genericOnClick(info: OnClickData, tab: chrome.tabs.Tab | undefine
           if (!session && !usingSSO) {
             const csrfToken = await getCsrfTokenFetch(baseUrl);
 
-            await performLoginOrLogoutFetch(`${baseUrl}/api/v1/auth/callback/credentials`, {
-              csrfToken: csrfToken,
-              callbackUrl: `${baseUrl}/api/v1/auth/callback`,
-              json: true,
-              redirect: false,
-              username: username,
-              password: password,
-            })
+            await performLoginOrLogoutFetch(
+              `${baseUrl}/api/v1/auth/callback/credentials`,
+              {
+                csrfToken: csrfToken,
+                callbackUrl: `${baseUrl}/api/v1/auth/callback`,
+                json: true,
+                redirect: false,
+                username: username,
+                password: password,
+              }
+            );
           } else if (!session && usingSSO) {
             return;
           }
@@ -215,22 +249,21 @@ async function genericOnClick(info: OnClickData, tab: chrome.tabs.Tab | undefine
           const newLink = await postLinkFetch(baseUrl, {
             url: tab.url,
             collection: {
-              name: "Unorganized",
+              name: 'Unorganized',
             },
             tags: [],
             name: tab.title,
             description: tab.title,
           });
 
-          const newLinkJson = await newLink.json()
+          const newLinkJson = await newLink.json();
           const newLinkUrl: bookmarkMetadata = newLinkJson.response;
           newLinkUrl.bookmarkId = tab.id?.toString();
 
-          await saveBookmarkMetadata(newLinkUrl)
+          await saveBookmarkMetadata(newLinkUrl);
         } catch (error) {
           console.error(error);
         }
-
       }
   }
 }
@@ -243,14 +276,14 @@ browser.runtime.onInstalled.addListener(function () {
     'editable',
     'image',
     'video',
-    'audio'
+    'audio',
   ];
   for (const context of contexts) {
-    const  title: string = "Add link to Linkwarden";
+    const title: string = 'Add link to Linkwarden';
     browser.contextMenus.create({
       title: title,
       contexts: [context],
-      id: context
+      id: context,
     });
   }
 });
@@ -259,71 +292,139 @@ browser.runtime.onInstalled.addListener(function () {
 
 browser.omnibox.onInputStarted.addListener(async () => {
   const configured = await isConfigured();
-  const description = configured ? 'Search links in linkwarden' : 'Please configure the extension first';
+  const description = configured
+    ? 'Search links in linkwarden'
+    : 'Please configure the extension first';
 
   browser.omnibox.setDefaultSuggestion({
     description: description,
   });
 });
 
-browser.omnibox.onInputChanged.addListener(async (text: string, suggest: (arg0: { content: string; description: string; }[]) => void) => {
-  const configured = await isConfigured();
+browser.omnibox.onInputChanged.addListener(
+  async (
+    text: string,
+    suggest: (arg0: { content: string; description: string }[]) => void
+  ) => {
+    const configured = await isConfigured();
 
+    if (!configured) {
+      return;
+    }
+
+    const currentBookmarks = await getBookmarksMetadata();
+
+    const searchedBookmarks = currentBookmarks.filter((bookmark) => {
+      return bookmark.name?.includes(text) || bookmark.url.includes(text);
+    });
+
+    const bookmarkSuggestions = searchedBookmarks.map((bookmark) => {
+      return {
+        content: bookmark.url,
+        description: bookmark.name || bookmark.url,
+      };
+    });
+    suggest(bookmarkSuggestions);
+  }
+);
+
+// This part was taken https://github.com/sissbruecker/linkding-extension/blob/master/src/background.js Thanks to @sissbruecker
+
+browser.omnibox.onInputEntered.addListener(
+  async (content: string, disposition: OnInputEnteredDisposition) => {
+    if (!(await isConfigured()) || !content) {
+      return;
+    }
+
+    const isUrl = /^http(s)?:\/\//.test(content);
+    const url = isUrl ? content : `lk`;
+
+    // Edge doesn't allow updating the New Tab Page (tested with version 117).
+    // Trying to do so will throw: "Error: Cannot update NTP tab."
+    // As a workaround, open a new tab instead.
+    if (disposition === 'currentTab') {
+      const tabInfo = await getCurrentTabInfo();
+      if (tabInfo.url === 'edge://newtab/') {
+        disposition = 'newForegroundTab';
+      }
+    }
+
+    switch (disposition) {
+      case 'currentTab':
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await browser.tabs.update({ url });
+        break;
+      case 'newForegroundTab':
+        await browser.tabs.create({ url });
+        break;
+      case 'newBackgroundTab':
+        await browser.tabs.create({ url, active: false });
+        break;
+    }
+  }
+);
+
+browser.webNavigation.onCompleted.addListener(async (evt) => {
+  // Filter out any sub-frame related navigation event
+  if (evt.frameId !== 0) {
+    return;
+  }
+  const url = new URL(evt.url);
+  console.log('Page loaded:', url);
+
+  const { syncBookmarks, baseUrl, username, password, usingSSO } =
+    await getConfig();
+  const configured = await isConfigured();
   if (!configured) {
     return;
   }
 
-  const currentBookmarks = await getBookmarksMetadata();
+  const session = await getSessionFetch(baseUrl);
 
-  const searchedBookmarks = currentBookmarks.filter(bookmark => {
-    return bookmark.name?.includes(text) || bookmark.url.includes(text);
-  });
+  if (!session && !usingSSO) {
+    const csrfToken = await getCsrfTokenFetch(baseUrl);
 
-  const bookmarkSuggestions = searchedBookmarks.map(bookmark => {
-     return {
-       content: bookmark.url,
-       description: bookmark.name || bookmark.url
-     }
-  });
-  suggest(bookmarkSuggestions)
-
-});
-
-// This part was taken https://github.com/sissbruecker/linkding-extension/blob/master/src/background.js Thanks to @sissbruecker
-
-browser.omnibox.onInputEntered.addListener(async (content: string, disposition: OnInputEnteredDisposition) => {
-  if (!await isConfigured() || !content) {
+    await performLoginOrLogoutFetch(
+      `${baseUrl}/api/v1/auth/callback/credentials`,
+      {
+        csrfToken: csrfToken,
+        callbackUrl: `${baseUrl}/api/v1/auth/callback`,
+        json: true,
+        redirect: false,
+        username: username,
+        password: password,
+      }
+    );
+  } else if (!session && usingSSO) {
     return;
   }
 
-  const isUrl = /^http(s)?:\/\//.test(content);
-  const url = isUrl
-    ? content
-    : `lk`;
+  const result = await getLinkFetch(baseUrl, url.href);
+  console.log(result);
 
-  // Edge doesn't allow updating the New Tab Page (tested with version 117).
-  // Trying to do so will throw: "Error: Cannot update NTP tab."
-  // As a workaround, open a new tab instead.
-  if (disposition === "currentTab") {
-    const tabInfo = await getCurrentTabInfo();
-    if (tabInfo.url === "edge://newtab/") {
-      disposition = "newForegroundTab";
-    }
+  if (result.response.length >= 1) {
+    // manually checking if the link actually exists because api
+    // returns approximate results
+    result.response.forEach((link: any) => {
+      if (link.url == url.href) {
+        console.log('Link found');
+        setIcon('./saved.jpg');
+      }
+    });
+  } else {
+    console.log('Link not found');
+    setIcon('./48.png');
   }
 
-  switch (disposition) {
-    case "currentTab":
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      await browser.tabs.update({ url });
-      break;
-    case "newForegroundTab":
-      await browser.tabs.create({ url });
-      break;
-    case "newBackgroundTab":
-      await browser.tabs.create({ url, active: false });
-      break;
-  }
+  console.log(syncBookmarks);
 });
 
-
+const setIcon = (imgPath: string) => {
+  if (getBrowser() === chrome) {
+    browser.action.setIcon({ path: imgPath });
+  } else {
+    // firefox uses manifest v2
+    browser.browserAction.setIcon({ path: imgPath });
+  }
+};
