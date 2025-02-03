d064674f9511504c85eb5f63d4071a4afb71294f
main <main@localhost>
2025-02-03 12:40:31 +0100

    Show if link is already added

--- src/@/lib/actions/links.ts
+++ src/@/lib/actions/links.ts
@@ -2,6 +2,7 @@ import captureScreenshot from '../screenshot.ts';
 import { bookmarkFormValues } from '../validators/bookmarkForm.ts';
 import axios from 'axios';
 import { bookmarkMetadata } from '../cache.ts';
+import { getCurrentTabInfo } from '../utils.ts';
 
 export async function postLink(
   baseUrl: string,
@@ -110,4 +111,25 @@ export async function getLinksFetch(
     },
   });
   return await response.json();
+}
+
+export async function checkLinkExists(
+  baseUrl: string,
+  apiKey: string
+): Promise<boolean> {
+  const tabInfo = await getCurrentTabInfo();
+  if (!tabInfo.url) {
+    console.error('No URL found for current tab');
+    return false;
+  }
+  const safeUrl = encodeURIComponent(tabInfo.url);
+  const url = `${baseUrl}/api/v1/links?searchQueryString=${safeUrl}&searchByUrl=true`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  });
+  const data = await response.json();
+  const exists = data.response.length > 0;
+  return exists;
 }
